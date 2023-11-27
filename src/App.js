de1d5602ccc52cb2883d57567b30a9074d615c9b
Glory ann Conwi <gaconwi@googlemail.com>
2023-11-27 13:25:50 +0100

    1st challenge, 3rd task, done.

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+  
+
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Pure relaxation</h2>
+      <label htmlFor="name">date:</label>
+      <input type="text" id="name" />
+      <a href="https://wellnest.me/standort/hamburg/" className="article__link">book now!</a>
+    </article>
+  );
 }
